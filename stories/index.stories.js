301f9ce6adbbf40c8e8092048508bd6787aa31d1
solsen <spam2steve@gmail.com>
2018-03-01 20:15:48 -0500

    add "currentTicker" class to number span in view; use storybook knobs

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,5 +1,16 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Ticker from "../lib/index";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobsOptions, text } from '@storybook/addon-knobs/react';
 
-storiesOf("Ticker", module).add("Ticker", () => <Ticker text="123.44" textClassName="text" />);
+import Ticker from '../lib/index';
+
+const stories = storiesOf('Storybook', module);
+
+stories.addDecorator(withKnobsOptions({
+  debounce: { wait: 200, leading: false }, // Same as lodash debounce.
+  timestamps: true // Doesn't emit events while user is typing.
+}));
+
+stories.add('Ticker (static)', () => <Ticker text="123.44" textClassName="text" />);
+
+stories.add('Ticker (with Knobs)', () => <Ticker text={text('ticker text', '123.456')} textClassName="text" />);
